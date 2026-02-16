3c423468220d1275fa887b842c2b516f441b6a3a
gargbydesign <amit.garg@kellogg.northwestern.edu>
2026-02-16 00:48:57 +0000

    created cards route,controller,action,view file blank

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
   get("/companies", :controller => "companies", :action => "index")
+  get("/cards", :controller => "cards", :action => "index")
 end
